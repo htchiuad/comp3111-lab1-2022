2748dac6e86445af41d3243f4338446167b3e1f3
user <user@10.89.28.5>
2022-02-22 16:03:35 +0800

    I have made a change

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -7,9 +7,10 @@ public class Library {
 	
 	/* Add this function */
 	public static void main(String arg[]) {
-		System.out.println("Hello, World!");
+		Library lib = nwe Library();
+		if(lib.someLibraryMethod())
+			System.out.println("Hello, World!");
 	}
-	
 	
     public boolean someLibraryMethod() {
         return true;
